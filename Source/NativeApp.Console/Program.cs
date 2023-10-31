80fc7903fb3af3779005f4145a72b114a5562c39
Eivind Fasting <micro__26@hotmail.com>
2023-10-31 08:48:28 +0100

    re-structured program.cs

--- Source/NativeApp.Console/Program.cs
+++ Source/NativeApp.Console/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using SuperOffice.Security.Principal;
+using System;
 
 namespace NativeAppConsole
 {
@@ -12,59 +13,64 @@ namespace NativeAppConsole
         public static async Task Main(string[] args)
         {
             using var host = CreateHostBuilder(args).Build();
+            
             var serviceProvider = host.Services;
             serviceProvider.RegisterWithNetServer();
+            
             await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-    Host.CreateDefaultBuilder(args)
-        .ConfigureAppConfiguration((context, config) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(ConfigureAppConfiguration)
+                .ConfigureServices(ConfigureServices)
+                .ConfigureLogging(ConfigureLogging);
+
+        private static void ConfigureAppConfiguration(HostBuilderContext context, IConfigurationBuilder config)
         {
             config.SetBasePath(Directory.GetCurrentDirectory())
                   .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-        })
-        .ConfigureServices((hostContext, services) =>
+        }
+
+        private static void ConfigureServices(HostBuilderContext hostContext, IServiceCollection services)
         {
             var configuration = hostContext.Configuration;
             var appSettings = configuration.GetSection("ApplicationSettings").Get<AppSettings>();
-            if (appSettings != null)
-            {
-                services.AddHostedService<AppLogic>();
-                services.AddSingleton<SystemBrowser>();
-                services.AddSingleton<OidcClient>(sp =>
-                {
-                    var browser = sp.GetRequiredService<SystemBrowser>();
-                    string redirectUri = $"http://127.0.0.1:{browser.Port}";
 
-                    var options = new OidcClientOptions
-                    {
-                        Authority = $"https://{appSettings.Environment}.superoffice.com",
-                        ClientId = appSettings.ClientId,
-                        RedirectUri = redirectUri,
-                        Scope = "openid profile",
-                        FilterClaims = false,
-                        Browser = browser,
-                        LoadProfile = false
-                    };
-
-                    return new OidcClient(options);
-                });
-                services.AddNetServerCore<ThreadContextProvider>();
-                services.AddServicesProxies();
-
-                // Add the configuration instance to the DI container
-                services.Configure<AppSettings>(configuration.GetSection("ApplicationSettings"));
-            }
-            else
-            {
+            if (appSettings == null)
                 throw new Exception("ApplicationSettings not found in appsettings.json");
-            }
-        })
-        .ConfigureLogging(logging =>
+
+            services.AddHostedService<AppLogic>()
+                    .AddSingleton<SystemBrowser>()
+                    .AddSingleton(sp => ConfigureOidcClient(sp, appSettings))
+                    .AddNetServerCore<ThreadContextProvider>()
+                    .AddServicesProxies()
+                    .Configure<AppSettings>(configuration.GetSection("ApplicationSettings"));
+        }
+
+        private static OidcClient ConfigureOidcClient(IServiceProvider sp, AppSettings appSettings)
         {
-            logging.ClearProviders();
-            logging.AddConsole();
-        });
+            var browser = sp.GetRequiredService<SystemBrowser>();
+            string redirectUri = $"http://127.0.0.1:{browser.Port}";
+
+            var options = new OidcClientOptions
+            {
+                Authority = $"https://{appSettings.Environment}.superoffice.com",
+                ClientId = appSettings.ClientId,
+                RedirectUri = redirectUri,
+                Scope = "openid profile",
+                FilterClaims = false,
+                Browser = browser,
+                LoadProfile = false
+            };
+
+            return new OidcClient(options);
+        }
+
+        private static void ConfigureLogging(ILoggingBuilder logging)
+        {
+            logging.ClearProviders()
+                   .AddConsole();
+        }
     }
 }
